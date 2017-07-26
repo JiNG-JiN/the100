c5300e85341b775f1229885e34c1114c9469237b
jung <jing.jin.jp@gmail.com>
2017-07-26 20:24:18 +0800

    implments the 100

--- assets/main/js/main.js
+++ assets/main/js/main.js
@@ -16,7 +16,8 @@ if (!!document.createElement('video').canPlayType) {
     //-------------------------------------------------------------------------------------
     // This code loads the IFrame Player API code asynchronously.
     var tag = document.createElement('script');
-    tag.src = "https://www.youtube.com/iframe_api";
+    //tag.src = "https://www.youtube.com/iframe_api";
+	tag.src = '';
     var firstScriptTag = document.getElementsByTagName('script')[0];
     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
